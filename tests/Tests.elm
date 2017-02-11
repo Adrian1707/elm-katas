911f34f1145f35f4ac6e2cd6bcd8f5a69f6659c9
Adrian Booth <adrian.booth17@gmail.com>
2017-02-11 17:58:54 +0000

    add functions to get elements of list by index

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -58,39 +58,90 @@ penultimate list =
             Just y
 
 
+elementAt : List a -> Int -> Maybe a
+elementAt xs n =
+    let
+        arr =
+            Array.fromList xs
+    in
+        Array.get (n - 1) arr
+
+
+elementAtRecursive : List a -> Int -> Maybe a
+elementAtRecursive list n =
+    case List.drop (n - 1) list of
+        [] ->
+            Nothing
+
+        y :: ys ->
+            Just y
+
+
 all : Test
 all =
     describe "Sample Test Suite"
-        [ describe "Unit test examples"
-            [ test "Addition" <|
-                \() ->
-                    Expect.equal (sublist 3 7 (List.range 1 10)) [ 3, 4, 5, 6, 7 ]
-            , test "String.left" <|
-                \() ->
-                    Expect.equal (sublist 3 -4 [ -3, -2, -1, 0, 1, 2, 3 ]) ([])
-            , test "sortByListLengths" <|
-                \() ->
-                    Expect.equal (sortByListLengths [ [ 1 ], [ 3 ], [ 2, 4 ], [ 4, 1, 5 ], [ 5 ] ]) ([ 1, 1, 1, 2, 3 ])
-            , test "sortByListLengths" <|
-                \() ->
-                    Expect.equal (sortByListLengths [ [ 1, 4, 5, 1 ], [ 3 ], [ 2, 4 ], [ 4, 1, 5 ], [ 5 ] ]) ([ 1, 1, 2, 3, 4 ])
-            , test "isNotPrime" <|
-                \() ->
-                    Expect.equal (isPrime 64) False
-            , test "isNotPrime" <|
-                \() ->
-                    Expect.equal (isPrime 144) False
-            , test "Returns the last in a list" <|
-                \() ->
-                    Expect.equal (last [ 3, 2, 4, 5 ]) (Just (5))
-            , test "Returns the last in a list" <|
-                \() ->
-                    Expect.equal (last [ 3, 2, 4, 5, 8 ]) (Just (8))
-            , test "Returns the penultimate in a list" <|
-                \() ->
-                    Expect.equal (penultimate [ 3, 2, 4, 5, 8 ]) (Just (5))
-              -- , test "isPrime" <|
-              --     \() ->
-              --         Expect.equal (isPrime 131) True
-            ]
+        firstUnitTests
+
+
+
+-- another : Test
+-- another =
+--     describe "Second test suite"
+--         secondUnitTests
+
+
+firstUnitTests =
+    [ describe "Unit test examples"
+        [ test "Addition" <|
+            \() ->
+                Expect.equal (sublist 3 7 (List.range 1 10)) [ 3, 4, 5, 6, 7 ]
+        , test "String.left" <|
+            \() ->
+                Expect.equal (sublist 3 -4 [ -3, -2, -1, 0, 1, 2, 3 ]) ([])
+        , test "sortByListLengths" <|
+            \() ->
+                Expect.equal (sortByListLengths [ [ 1 ], [ 3 ], [ 2, 4 ], [ 4, 1, 5 ], [ 5 ] ]) ([ 1, 1, 1, 2, 3 ])
+        , test "sortByListLengths" <|
+            \() ->
+                Expect.equal (sortByListLengths [ [ 1, 4, 5, 1 ], [ 3 ], [ 2, 4 ], [ 4, 1, 5 ], [ 5 ] ]) ([ 1, 1, 2, 3, 4 ])
+        , test "isNotPrime" <|
+            \() ->
+                Expect.equal (isPrime 64) False
+        , test "isNotPrime" <|
+            \() ->
+                Expect.equal (isPrime 144) False
+        , test "Returns the last in a list" <|
+            \() ->
+                Expect.equal (last [ 3, 2, 4, 5 ]) (Just (5))
+        , test "Returns the last in a list" <|
+            \() ->
+                Expect.equal (last [ 3, 2, 4, 5, 8 ]) (Just (8))
+        , test "Returns the penultimate in a list" <|
+            \() ->
+                Expect.equal (penultimate [ 3, 2, 4, 5, 8 ]) (Just (5))
+        , elementAtTest
+        , elementAtTestWithJust
+        , elementAtTestRecursive
+          -- , test "isPrime" <|
+          --     \() ->
+          --         Expect.equal (isPrime 131) True
         ]
+    ]
+
+
+elementAtTest =
+    test "elementAt with Nothing" <|
+        \() ->
+            Expect.equal (elementAt [ 3, 2, 4, 1 ] 6) Nothing
+
+
+elementAtTestWithJust =
+    test "elementAt with Just value" <|
+        \() ->
+            Expect.equal (elementAt [ 3, 2, 4, 1 ] 3) (Just (4))
+
+
+elementAtTestRecursive =
+    test "elementAt using recursion" <|
+        \() ->
+            Expect.equal (elementAtRecursive [ 3, 2, 4, 1 ] 1) (Just (3))
